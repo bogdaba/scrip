f5ecf2faf7a539b128824e4a4110cc94f350823e
bogdaba <bogdaba@github.com>
2023-08-01 14:23:00 +0200

    rewrote some things

--- nhc.ps1
+++ nhc.ps1
@@ -1,3 +1,5 @@
+# irm https://raw.githubusercontent.com/bogdaba/scrip/main/nhc.ps1 | iex
+
 # Getting user input for URL
 $url = Read-Host "Please enter URL to collect stats for"
 
@@ -21,32 +23,53 @@ function Write-ToFile {
 }
 
 # Write info to file
+Write-Output "Writing user information to file..."
 Write-ToFile "***********************"
 Write-ToFile "Username=$username"
 Write-ToFile "***********************"
 
 # Running commands and capturing output
+Write-Output "Collecting IP configuration..."
 Write-ToFile (ipconfig /all)
 Write-ToFile "***********************"
+
+Write-Output "Collecting network routing tables..."
 Write-ToFile (netstat -rn)
 Write-ToFile "***********************"
+
+Write-Output "Collecting workstation statistics..."
 Write-ToFile "Uptime and Statistics"
 Write-ToFile (net statistics workstation)
 Write-ToFile "***********************"
-Write-ToFile (ping -w 1 -n 5 $url)
+
+Write-Output "Pinging specified URL..."
+Start-Job -ScriptBlock {param($url) ping -w 1 -n 5 $url} -ArgumentList $url | Wait-Job -Timeout 10 | Receive-Job
 Write-ToFile "***********************"
-Write-ToFile (ping -w 1 -n 5 'whatismyip.com')
+
+Write-Output "Pinging 'whatismyip.com'..."
+Start-Job -ScriptBlock {ping -w 1 -n 5 'whatismyip.com'} | Wait-Job -Timeout 10 | Receive-Job
 Write-ToFile "***********************"
-Write-ToFile (ping -w 1 -n 5 'www.google.com')
+
+Write-Output "Pinging 'www.google.com'..."
+Start-Job -ScriptBlock {ping -w 1 -n 5 'www.google.com'} | Wait-Job -Timeout 10 | Receive-Job
 Write-ToFile "***********************"
-Write-ToFile (tracert -w 1 $url)
+
+Write-Output "Tracing route to specified URL..."
+Start-Job -ScriptBlock {param($url) tracert -w 1 $url} -ArgumentList $url | Wait-Job -Timeout 60 | Receive-Job
 Write-ToFile "***********************"
+
+Write-Output "Collecting PAC file information..."
 Write-ToFile "PAC File"
 Write-ToFile ((Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name AutoConfigURL).AutoConfigURL)
 Write-ToFile "***********************"
+
+Write-Output "Collecting Proxy settings..."
 Write-ToFile "Proxy Settings"
-Write-ToFile ((Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer).ProxyServer)
-Write-ToFile "***********************"
+$proxySettings = netsh winhttp show proxy
+Write-ToFile $proxySettings
+
+
+Write-Output "Collecting system hotfix information..."
 Write-ToFile (Get-HotFix)
 
 Write-Output "Congrats! Your script executed successfully."
