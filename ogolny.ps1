ab22d9787f2acf0642bffc9822e9746c927dc300
bogdaba <bogdaba@github.com>
2023-08-01 15:06:45 +0200

    nyo

---
+++ ogolny.ps1
@@ -0,0 +1,31 @@
+# irm https://raw.githubusercontent.com/bogdaba/scrip/main/ogolny.ps1 | iex
+
+# Define array of commands
+$commands = @(
+    @{Cmd = "wuauclt /detectnow /updatenow"; Desc = "Windows Update"},
+    @{Cmd = "sfc /scannow"; Desc = "System File Check"},
+    @{Cmd = "DISM /Online /Cleanup-Image /CheckHealth"; Desc = "DISM CheckHealth"},
+    @{Cmd = "DISM /Online /Cleanup-Image /ScanHealth"; Desc = "DISM ScanHealth"},
+    @{Cmd = "DISM /Online /Cleanup-Image /RestoreHealth"; Desc = "DISM RestoreHealth"},
+    @{Cmd = "chkdsk c: /f /r"; Desc = "Check Disk C:"},
+    @{Cmd = "chkdsk d: /f /r"; Desc = "Check Disk D:"},
+    @{Cmd = "ipconfig /flushdns"; Desc = "Flush DNS"},
+    @{Cmd = "ipconfig /registerdns"; Desc = "Register DNS"},
+    @{Cmd = "ipconfig /release"; Desc = "IP Release"},
+    @{Cmd = "ipconfig /renew"; Desc = "IP Renew"},
+    @{Cmd = "gpupdate /force"; Desc = "Force Group Policy Update"}
+)
+
+# Loop through each command
+foreach ($command in $commands) {
+    Write-Host "Starting $($command.Desc)..."
+    try {
+        # Execute the command
+        Invoke-Expression $command.Cmd | Out-Host
+        Write-Host "$($command.Desc) completed successfully."
+    } catch {
+        Write-Host "An error occurred during $($command.Desc)."
+        Write-Host $_.Exception.Message
+    }
+    Write-Host ""
+}
